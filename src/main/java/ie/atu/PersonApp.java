325a28ecbb9a48a1dc876d1872dd47d445104b93
Olamide Aderogba <g00401971@atu.ie>
2022-11-29 13:45:40 +0000

    Added new Class called Person App

---
+++ src/main/java/ie/atu/PersonApp.java
@@ -0,0 +1,8 @@
+package ie.atu;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person richard = new Person("Richard","Galway","12345");
+        System.out.println(richard.toString());
+    }
+}
